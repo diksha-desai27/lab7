bf743710efe573a3a2e811b1a908899df7e3ed8b
Diksha Desai <desai.di@husky.neu.edu>
2020-02-22 11:35:35 -0500

    Get 5 invalid user based on posts

--- lab7_complete/src/lab7/analytics/AnalysisHelper.java
+++ lab7_complete/src/lab7/analytics/AnalysisHelper.java
@@ -10,8 +10,10 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import lab7.entities.Comment;
 import lab7.entities.Post;
 import lab7.entities.User;
@@ -94,4 +96,35 @@ public class AnalysisHelper {
         System.out.println("Post "+ maxId + " has most comments.");
         System.out.println("Comments of post id: " + maxId + " " + "are" + posts.get(maxId).getComments());
     }
+    
+    //Top 5 inactive users based on total posts numbers.
+    public void getInactiveUsersBasedOnPosts() {
+        Map<Integer, Post> posts = DataStore.getInstance().getPosts();
+        Map<Integer, User> users = DataStore.getInstance().getUsers();
+        Map<Integer, User> inactiveUsers = new HashMap<>();
+        
+        for(User u: users.values()) {
+            int count = 0;
+            for(Post p: posts.values()) {
+                if(p.getUserId() == u.getId()) {
+                    count++;
+                }
+            }
+            inactiveUsers.put(count, u);
+        }
+        
+        Set set = inactiveUsers.entrySet();
+        Iterator it = set.iterator();
+        int i = 0;
+        while (it.hasNext() && i < 5) {
+            Map.Entry me = (Map.Entry) it.next();
+            System.out.print("Key is: " + me.getKey() + " & ");
+            System.out.println("Value is: " + me.getValue());
+            i++;
+        }
+
+    }
+    
+    
+    
 }
